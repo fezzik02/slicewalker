713555ffaf2303a9cc6840f3500d8d60f4ba83a2
John Garcia <fezzik02@gmail.com>
2023-01-26 12:45:01 -0800

    Fix indexing

--- slicewalker.py
+++ slicewalker.py
@@ -14,22 +14,28 @@ args = parser.parse_args()
 # Pull in the image once and read it into a list of RGB tuples
 def process_image(image):
     strips = []
-    with Image.open('images/attachment-1.jpeg').convert("RGB") as im:
-        im_length = im.getbbox()[3]
+    with Image.open('images/attachment-3.jpeg').convert("RGB") as im:
+        im_length = im.getbbox()[2]
         for i in range(im_length):
             pixels = []
-            for i in range(60):
-                pixels.append(im.getpixel((0,i)))
+            for j in range(60):
+                pixels.append(im.getpixel((i,j)))
             strips.append(pixels)
     return strips
 
 def write_strips(data):
-    strip = neopixel.NeoPixel(board.args.pin, args.length)
-    for i in range(data.length()):
-        strip[0:0] = i
+    strip = neopixel.NeoPixel(board.D18, 60)
+    strip = []
+    for i in range(len(data)):
+        # for j in range(len(data[i])):
+        strip[0:0] = data[i]
+        # print(strip)
         strip.show()
+        # pass
         sleep(2)
 
 if __name__ == '__main__':
     strips = process_image(args.image)
+    # print(strips)
+    print(len(strips))
     write_strips(strips)
